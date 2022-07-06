c64279c176b3cd46dc9351490a4fbfcd08a3e332
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-07-06 12:38:47 -0700

    Completed javascript-data-model.

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,21 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  }
+];
+
+console.log('the second book is:', bookshelf[0].title + ' by ' + bookshelf[0].author);
+console.log('the first books ISBN is:', bookshelf[0].isbn);
+console.log('the title of the third book is:', bookshelf[2].title);
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,55 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    datePlaced: 'August 4, 2020',
+    dateDelivered: 'August 8, 2020',
+    totalPrice: '$34.00',
+    shippedTo: 'JS Masher',
+    book: {
+      author: 'Dr. Axel Raushmayer',
+      title: 'JavaScript for impatient programmers',
+      price: '$31.55'
+    }
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    datePlaced: 'July 19, 2020',
+    dateDelivered: 'July 20, 2020',
+    totalPrice: '$44.53',
+    shippedTo: 'JS Masher',
+    book: {
+      author: 'Christopher Alexander',
+      title: 'The Timeless Way of Building',
+      price: '$41.33'
+    }
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    datePlaced: 'July 4, 2020',
+    dateDelivered: 'July 7, 2020',
+    totalPrice: '$17.22',
+    shippedTo: 'JS Masher',
+    gameEquipment: {
+      console: 'GameCube',
+      equipmentType: 'Controller Adapter',
+      price: '$15.98'
+    }
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    datePlaced: 'July 3, 2020',
+    dateDelivered: 'July 5, 2020',
+    totalPrice: '$138.93',
+    shippedTo: 'JS Masher',
+    gameEquipment: {
+      console: 'GameCube',
+      equipmentType: 'GameCube controller',
+      price: '$94.95'
+    },
+    book: {
+      author: 'Stephanie Faroult',
+      title: 'The Art of SQL',
+      price: '$33.99'
+    }
+  }
+];

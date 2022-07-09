3415b0833eb3b5c906e721507cb0f64eafea49a4
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-07-09 14:01:59 -0700

    fixed formatting.

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -7,56 +7,56 @@
             getValues
  */
 
-            function getNumbersToTen() {
-              var numbers = [];
-              var currentNumber = 1;
-              while(currentNumber <= 10) {
-                numbers.push(currentNumber);
-                currentNumber++;
-              } return numbers;
-            }
+function getNumbersToTen() {
+  var numbers = [];
+  var currentNumber = 1;
+  while(currentNumber <= 10) {
+    numbers.push(currentNumber);
+    currentNumber++;
+  } return numbers;
+}
 
-            function getEvenNumbersToTwenty() {
-              var evenNumbers = [];
-              var currentNumber = 2;
-              while(currentNumber <= 20) {
-                evenNumbers.push(currentNumber);
-                currentNumber+=2;
-              } return evenNumbers;
-            }
+function getEvenNumbersToTwenty() {
+  var evenNumbers = [];
+  var currentNumber = 2;
+  while(currentNumber <= 20) {
+    evenNumbers.push(currentNumber);
+    currentNumber+=2;
+  } return evenNumbers;
+}
 
-            function repeatWord(word, times) {
-              var count = 1;
-              var repeated = '';
-              while(count < times) {
-                count++;
-                repeated += word
-              } return repeated;
-            }
+function repeatWord(word, times) {
+  var count = 1;
+  var repeated = '';
+  while(count < times) {
+    count++;
+    repeated += word
+  } return repeated;
+}
 
-            function logEachCharacter(string) {
-              for(var i = 0; i < string.length; i++) {
-                console.log(string[i]);
-              }
-            }
+function logEachCharacter(string) {
+  for(var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
 
-            function doubleAll(numbers) {
-              var doubled = [];
-              for(var i = 0; i < numbers.length; i++) {
-                doubled.push(numbers[i] * 2);
-              }return doubled;
-            }
+function doubleAll(numbers) {
+  var doubled = [];
+  for(var i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }return doubled;
+}
 
-            function getKeys(object) {
-              var keys = [];
-              for(var key in object) {
-                keys.push(key);
-              }return keys;
-            }
+function getKeys(object) {
+  var keys = [];
+  for(var key in object) {
+    keys.push(key);
+  }return keys;
+}
 
-            function getValues(object) {
-              var values = [];
-              for(var key in object) {
-                values.push(object[key]);
-              }return values
-            }
+function getValues(object) {
+  var values = [];
+  for(var key in object) {
+    values.push(object[key]);
+  }return values
+}

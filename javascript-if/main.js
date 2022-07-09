6e96acb39cb3301640cd4ec988800f57db690462
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-07-09 13:59:16 -0700

    fixed the weird indentation.

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -8,68 +8,68 @@
             introduceWarnerBro
  */
 
-            function isUnderFive(number) {
-              if(number < 5) {
-                return true;
-              } else{
-                return false;
-              }
-            }
+function isUnderFive(number) {
+  if(number < 5) {
+    return true;
+  } else{
+    return false;
+  }
+}
 
-            function isEven(number) {
-              if(number % 2 === 0) {
-                return true;
-              } else{
-                return false;
-              }
-            }
+function isEven(number) {
+  if(number % 2 === 0) {
+    return true;
+  } else{
+    return false;
+  }
+}
 
-            function startsWithJ(string) {
-              if(string.charAt(0) === 'J') {
-                return true;
-              } else{
-                return false;
-              }
-            }
+function startsWithJ(string) {
+  if(string.charAt(0) === 'J') {
+    return true;
+  } else{
+    return false;
+  }
+}
 
-            function isOldEnoughToDrink(person) {
-              if(person.age >= 21) {
-                return true;
-              } else{
-                return false;
-              }
-            }
+function isOldEnoughToDrink(person) {
+  if(person.age >= 21) {
+    return true;
+  } else{
+    return false;
+  }
+}
 
-            function isOldEnoughToDrive(person) {
-              if(person.age >= 16) {
-                return true;
-              } else{
-                return false;
-              }
-            }
+function isOldEnoughToDrive(person) {
+  if(person.age >= 16) {
+    return true;
+  } else{
+    return false;
+  }
+}
 
-            function isOldEnoughToDrinkAndDrive(person) {
-              return false;
-            }
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
 
-            function categorizeAcidity(pH) {
-              if(pH === 7){
-                return 'neutral';
-              } if((pH >= 0) && (pH < 7)) {
-                return 'acid';
-              } if((pH <= 14) && (pH > 7)) {
-                return 'base';
-              } else{
-                return 'invalid pH level';
-              }
-            }
+function categorizeAcidity(pH) {
+  if(pH === 7){
+    return 'neutral';
+  } else if((pH >= 0) && (pH < 7)) {
+    return 'acid';
+  } else if((pH <= 14) && (pH > 7)) {
+    return 'base';
+  } else{
+    return 'invalid pH level';
+  }
+}
 
-            function introduceWarnerBro(name) {
-              if((name === 'yakko') || (name === 'wakko')) {
-                return "We're the warner brothers!";
-              } if(name === 'dot') {
-                return "I'm cute~";
-              } else{
-                return 'Goodnight everybody!'
-              }
-            }
+function introduceWarnerBro(name) {
+  if((name === 'yakko') || (name === 'wakko')) {
+    return "We're the warner brothers!";
+  } else if(name === 'dot') {
+    return "I'm cute~";
+  } else{
+    return 'Goodnight everybody!'
+  }
+}

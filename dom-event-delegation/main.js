9541798530e8a01fbb74553b31d928d7a3ff8b13
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-07-14 12:30:02 -0700

    Completed dom-event-delegation exercise.

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,15 @@
+var $taskList = document.querySelector('.task-list');
+
+function buttonClick(event){
+  console.log('event.target:', event.target);
+
+  console.log('event.target.tagName:', event.target.tagName);
+
+  if(event.target && event.target.tagName === 'BUTTON'){
+    var closest = event.target.closest('.task-list-item');
+    console.log('closest .task-list-item', closest);
+    closest.remove();
+  }
+}
+
+$taskList.addEventListener('click', buttonClick);

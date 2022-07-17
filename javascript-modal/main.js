f51dab6fffb7b80f3bf270f32b0b64fc2b644170
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-07-17 11:58:15 -0700

    removed inline styling and used className.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,13 +1,13 @@
 var $modal = document.querySelector('.modal');
 
-var $purpleButton =document.querySelector('.purple');
+var $purpleButton = document.querySelector('.purple');
 
 var $pinkButton = document.querySelector('.pink')
 
-$purpleButton.addEventListener('click', function purpleButton(event){
-  $modal.style.display = 'block';
+$purpleButton.addEventListener('click', function purpleButton(event) {
+  $modal.className = 'modal block-display';
 });
 
-$pinkButton.addEventListener('click', function pinkButton(event){
-  $modal.style.display = 'none';
+$pinkButton.addEventListener('click', function pinkButton(event) {
+  $modal.className = 'modal display-none';
 });

a537f4db00439f8ededd18916bbc1dadb33d2b8e
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-07-25 11:51:08 -0700

    Completed javascript-this exercise.

---
+++ javascript-this/student.js
@@ -0,0 +1,16 @@
+/* exported student */
+var student = {
+  firstName: 'Alvaro',
+
+  lastName: 'Diaz',
+
+  subject: 'JavaScript',
+
+  getFullName: function () {
+    return this.firstName + ' ' + this.lastName;
+  },
+
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+  }
+};

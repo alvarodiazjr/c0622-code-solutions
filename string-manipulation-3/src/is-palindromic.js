e65f0c69f02327284152189a4750a62c6172a0a7
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-11 03:33:19 +0000

    finished problems 1-3 still working on 4.

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,4 +1,15 @@
 /* exported isPalindromic */
-// function isPalindromic(string) {
+// store new string that removes spaces from string.
+// store new string that makes it into an array and reverses it.
+// put array back into string and remove spaces.
+// if both new strings are equal return true or else return false.
 
-// }
+function isPalindromic(string) {
+  var stringArr = string.replace(' ', '');
+  var reversedString = string.split('').reverse().join('').replace(' ', '');
+  if (stringArr === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,10 @@
 /* exported reverseWords */
+// make a var to split the word at every space and reverse each word.
+// split that new var again and reverse it so that its in the original order
+// of the string.
+// retunr the new string.
+function reverseWords(word) {
+  var reversedWord = word.split('').reverse().join('');
+  var finalString = reversedWord.split(' ').reverse().join(' ');
+  return finalString;
+}

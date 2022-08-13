b5858caee7c4983bc5c94670a1cc499686324eda
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-13 00:01:22 +0000

    still working on zip.

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,10 @@
 /* exported zip */
+function zip(first, second) {
+}
+
+// if (first.length <= second.length) {
+//   var zip = first.map(function (lady, i) {
+//     return [lady, second[i]];
+//   });
+// }
+// return zip;

de04b2f0223f1fdf258a8f6b24c8272d42ac19f9
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-14 20:09:09 +0000

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,62 +1,74 @@
 /* exported titleCase */
 /*
-- Hard code all minor characters.
-- Make an empty arrray.
-- Turn title into an array of strings seperated by spaces.
-- loop through array
-- make the first letter of array uppercase no matter what.
-- If string length is greater than 4 or less than 3 and doesnt include any minor
-strings, uppercase first letter and lowercase the rest. and push strings into new array.
-- If the string matched one of the minor strings push it into new array with
-all letters lowercased.
-- If a string is following a colon always capitalize the first letter.
-- turn the new array back into a string.
-- If the string contains 'Javascript' replace it as 'JavaScript'.
-- If the string contains 'Api' replace it as 'API'.
-- return the string.
+- Hard code every minor word and put it into array.
+- make an empty string to hold all the changes made to the original string.
+- Original string is called title.
+- split title by its spaces and store it in a variable named stringArr.
+- Loop through the stringArr.
+- lower case every string in the array.
+- if the string includes a hyphen (-)
+- split the string by the hyphen.
+- then capitalize the first letter and lowercase the rest
+on both sides of the hyphen.
+- if the string includes the string (api).
+- replace it with the hardcoded string (API).
+- if the string includes the string (javascript).
+- replace it with the hardcoded string (JavaScript).
+- if the string is at index[0] or it doesnt include any minor words.
+- Capitalize first letter and lowercase the rest.
+- Add all changed strings into the empty finalString variable.
+- Split the finalString array by a colon(:) and store it in a variable
+named stringV2.
+- make a variable named newArray and have it store an empty array.
+- loop through stringV2.
+- if stringV2[index] is equal to 1 then capitalize first letter.
+- then push it into newArray.
+- if the stringV2 length is equal to 0 push it into newArray with no changes.
+- Join the array back into a string by the colon (:) and store it in a variable
+named titleCase.
+- check with console log if titleCase is giving the correct input.
+- return titleCase.
 */
+
 function titleCase(title) {
-  var minors = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
-  var array = title.split(' ');
+  var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+
+  var finalString = '';
+
+  var stringArr = title.split(' ');
+
+  for (var i = 0; i < stringArr.length; i++) {
+    var string = stringArr[i].toLowerCase();
+
+    if (string.includes('-')) {
+      var words = string.split('-');
+      var firstHalf = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+      var secondHalf = words[1].charAt(0).toUpperCase() + words[1].slice(1);
+      string = firstHalf + '-' + secondHalf;
+
+    } else if (string.includes('api')) {
+      string = string.replace('api', 'API');
+
+    } else if (string.includes('javascript')) {
+      string = string.replace('javascript', 'JavaScript');
+
+    } else if (!minorWords.includes(string) || i === 0) {
+      string = string[0].toUpperCase() + string.slice(1);
+    }
+    finalString += string + ' ';
+  }
+
+  var stringV2 = finalString.trim().split(': ');
+
   var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === array[0]) {
-      newArray.push(array[0].charAt(0).toUpperCase() + array[0].slice(1).toLowerCase());
-    } else if ((array[i].length >= 4 || array[i].length <= 3) && !minors.includes(array[i])) {
-      newArray.push(array[i].charAt(0).toUpperCase() + array[i].slice(1).toLowerCase());
+
+  for (var x = 0; x < stringV2.length; x++) {
+    if (stringV2[x] === stringV2[1]) {
+      newArray.push(stringV2[1].charAt(0).toUpperCase() + stringV2[1].slice(1));
     } else {
-      newArray.push(array[i].charAt(0).toLowerCase() + array[i].slice(1).toLowerCase());
+      newArray.push(stringV2[x]);
     }
   }
-  var final = newArray.join(' ');
-  var string = final.replace('Javascript', 'JavaScript');
-  var api = string.replace('Api', 'API');
-  var complete = [];
-
-  var colonSplit = api.split(': ');
-
-  for (var j = 0; j < colonSplit.length; j++) {
-    if (colonSplit[j] === colonSplit[0]) {
-      complete.push(colonSplit[j]);
-    } else if (!complete.includes(colonSplit[j])) {
-      complete.push(colonSplit[1].charAt(0).toUpperCase() + colonSplit[1].slice(1));
-    }
-  }
-
-  var titleCase = complete.join(': ');
-  var finalArray = [];
-
-  var hyphenSplit = titleCase.split('-');
-
-  for (var x = 0; x < hyphenSplit.length; x++) {
-    if (hyphenSplit[x] === hyphenSplit[0]) {
-      finalArray.push(hyphenSplit[x]);
-    } else if (!finalArray.includes(hyphenSplit[x])) {
-      finalArray.push(hyphenSplit[1].charAt(0).toUpperCase() + hyphenSplit[1].slice(1));
-    }
-  }
-
-  var finalString = finalArray.join('-');
-
-  return finalString;
+  var titleCase = newArray.join(': ');
+  return titleCase;
 }

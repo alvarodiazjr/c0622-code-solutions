b9c9d241c2c3234d701ce32432fa3197faebb036
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-18 20:42:56 -0700

    removed else if statements.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -2,14 +2,13 @@ var $countdown = document.querySelector('h1');
 
 
 function countdown() {
-  if ($countdown.textContent === '4') {
-    $countdown.textContent = '3';
-  } else if ($countdown.textContent === '3'){
-    $countdown.textContent = '2';
-  } else if ($countdown.textContent === '2'){
-    $countdown.textContent = '1';
-  } else if ($countdown.textContent === '1'){
+  var count = $countdown.textContent;
+  if (count > 1) {
+    count--;
+    $countdown.textContent = count;
+  } else {
     $countdown.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(count);
   }
 }
 

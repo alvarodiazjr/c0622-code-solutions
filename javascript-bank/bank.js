95ff78d2b61d6afeff7f585479e8309021469443
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-20 14:07:12 -0700

    Completed JavaScript-Bank Exercise.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -5,7 +5,7 @@ function Bank() {
 }
 
 Bank.prototype.openAccount = function(holder, balance) {
-  if(Number.isInteger(balance) && balance > 0){
+  if (Number.isInteger(balance) && balance > 0) {
     var newAccount = new Account(this.nextAccountNumber, holder);
     newAccount.deposit(balance)
     this.accounts.push(newAccount);
@@ -16,8 +16,8 @@ Bank.prototype.openAccount = function(holder, balance) {
 };
 
 Bank.prototype.getAccount = function(number) {
-  for(var i = 0; i < this.accounts.length; i++){
-    if(number === this.accounts[i].number){
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (number === this.accounts[i].number) {
       return this.accounts[i];
     }
   }
@@ -25,37 +25,9 @@ Bank.prototype.getAccount = function(number) {
 }
 
 Bank.prototype.getTotalAssets = function () {
-  var deposits = 0;
-  var withdraws = 0;
-  var total = deposits - withdraws;
-  var trans = [];
-  if(this.accounts.length === 0){
-    return 0;
+  var totalAssets = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    totalAssets += this.accounts[i].getBalance();
   }
-  for(var i = 0; i < this.accounts.length; i++){
-    trans.push(this.accounts[i].transactions);
-  }
-  for(var i = 0; i < trans.length; i++){
-    if(trans[i].type === 'deposit'){
-      deposits += trans[i].amount;
-    } else if(trans[i].type === 'withdrawal'){
-      withdraws += trans[i].amount;
-
-    }
-  }
-  return total;
+  return totalAssets;
 };
-
-// var bank = new Bank();
-// var one = bank.openAccount('Jeff Jefferson', 10);
-// var two = bank.openAccount('Jeff Jefferson Jr', 100);
-// var three = bank.openAccount('Jeff Jefferson III', 1000);
-// var first = bank.getAccount(one);
-// var second = bank.getAccount(two);
-// var third = bank.getAccount(three);
-// bank.getTotalAssets()
-// first.deposit(5);
-// second.deposit(15);
-// second.withdraw(32);
-// third.deposit(30);
-// bank.getTotalAssets()
--- javascript-bank/transaction.js
+++ javascript-bank/transaction.js
@@ -1,9 +1,9 @@
 /* exported Transaction */
 function Transaction(type, amount) {
-  if(type === 'deposit' || type === 'withdrawal'){
+  if (type === 'deposit' || type === 'withdrawal') {
     this.type = type;
   }
-  if(amount > 0 && Number.isInteger(amount)){
+  if (amount > 0 && Number.isInteger(amount)) {
     this.amount = amount;
   } else {
     this.amount = 'invalid number';

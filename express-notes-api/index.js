dee2e8ff68d5f25798ba23768a09d00dd21b1215
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-23 21:26:08 +0000

    changed put method to verify id is positive.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -50,7 +50,7 @@ app.post('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
   const num = parseInt(req.params.id, 10);
-  if (!Number.isInteger(num)) {
+  if (!Number.isInteger(num) || num < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (Number.isInteger(num) && !notes[num]) {
     res.status(404).json({ error: 'cannot find note with id ' + num });
@@ -68,7 +68,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
 app.put('/api/notes/:id', (req, res) => {
   const num = parseInt(req.params.id, 10);
-  if (!Number.isInteger(num)) {
+  if (!Number.isInteger(num) || num < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (!req.body.content) {
     res.status(400).json({ error: 'content is a required field' });

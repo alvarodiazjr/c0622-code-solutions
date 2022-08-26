afc154d926ba3463cbd40c8096dfb52a3daec46b
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-26 19:56:47 +0000

    added check for non-integer and gradeId of 0 is invalid.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -32,30 +32,28 @@ app.post('/api/grades', (req, res) => {
   const input = req.body;
   const name = input.name;
   const course = input.course;
-  const score = input.score;
-  if (score < 0 || score > 100 || Number.isNaN(score)) {
+  const score = Number(input.score);
+  if (!Number.isInteger(score) || score < 0 || score > 100) {
     res.status(400).json({ error: 'Score must be a positive integer between 0 and 100' });
-    return;
-  }
-  if (!name || !course || !score) {
+  } else if (!name || !course || score === undefined) {
     res.status(400).json({ error: 'Must include Name, Course, and Score.' });
-    return;
+  } else {
+    const sql = `
+      insert into "grades" ("name", "course", "score")
+        values ($1, $2, $3)
+      returning *
+    `;
+    const values = [name, course, score];
+    db.query(sql, values)
+      .then(result => {
+        const grade = result.rows[0];
+        res.status(201).json(grade);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occured' });
+      });
   }
-  const sql = `
-    insert into "grades" ("name", "course", "score")
-      values ($1, $2, $3)
-    returning *
-  `;
-  const values = [name, course, score];
-  db.query(sql, values)
-    .then(result => {
-      const grade = result.rows[0];
-      res.status(201).json(grade);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'An unexpected error occured' });
-    });
 });
 
 app.put('/api/grades/:gradeId', (req, res) => {
@@ -63,46 +61,42 @@ app.put('/api/grades/:gradeId', (req, res) => {
   const input = req.body;
   const name = input.name;
   const course = input.course;
-  const score = input.score;
-  if (!Number.isInteger(gradeId) || gradeId < 0) {
+  const score = Number(input.score);
+  if (!Number.isInteger(gradeId) || gradeId < 1) {
     res.status(400).json({ error: 'invalid gradeId input' });
-    return;
-  }
-  if (score < 0 || score > 100 || Number.isNaN(score)) {
+  } else if (!Number.isInteger(score) || score < 0 || score > 100) {
     res.status(400).json({ error: 'Score must be a positive integer between 0 and 100' });
-    return;
-  }
-  if (!name || !course || !score) {
+  } else if (!name || !course || score === undefined) {
     res.status(400).json({ error: 'Must include Name, Course, and Score.' });
-    return;
+  } else {
+    const sql = `
+      update "grades"
+        set  "name" = $1,
+            "course" = $2,
+            "score" = $3
+      where "gradeId" = $4
+      returning *
+    `;
+    const values = [name, course, score, gradeId];
+    db.query(sql, values)
+      .then(result => {
+        const grade = result.rows[0];
+        if (!grade) {
+          res.status(404).json({ error: 'cannot find grade with gradeId of ' + gradeId });
+        } else {
+          res.status(200).json(grade);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occured' });
+      });
   }
-  const sql = `
-    update "grades"
-      set  "name" = $1,
-          "course" = $2,
-          "score" = $3
-    where "gradeId" = $4
-    returning *
-  `;
-  const values = [name, course, score, gradeId];
-  db.query(sql, values)
-    .then(result => {
-      const grade = result.rows[0];
-      if (!grade) {
-        res.status(404).json({ error: 'cannot find grade with gradeId of ' + gradeId });
-      } else {
-        res.status(200).json(grade);
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'An unexpected error occured' });
-    });
 });
 
 app.delete('/api/grades/:gradeId', (req, res) => {
   const gradeId = Number(req.params.gradeId);
-  if (!Number.isInteger(gradeId) || gradeId < 0) {
+  if (!Number.isInteger(gradeId) || gradeId < 1) {
     res.status(400).json({ error: 'invalid gradeId input' });
     return;
   }

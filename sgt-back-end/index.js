b8d20212de1cef2c879a9b34ba8dc6b0861e724b
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-26 18:23:01 +0000

    removed destructed aray and changed score to work with 0.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -30,64 +30,74 @@ app.get('/api/grades', (req, res) => {
 
 app.post('/api/grades', (req, res) => {
   const input = req.body;
-  const [name, course, score] = [input.name, input.course, Number(input.score)];
+  const name = input.name;
+  const course = input.course;
+  const score = input.score;
+  if (score < 0 || score > 100 || Number.isNaN(score)) {
+    res.status(400).json({ error: 'Score must be a positive integer between 0 and 100' });
+    return;
+  }
   if (!name || !course || !score) {
     res.status(400).json({ error: 'Must include Name, Course, and Score.' });
-  } else if (!Number.isInteger(score) || score < 1 || score > 100) {
-    res.status(400).json({ error: 'Score must be a positive integer between 1 and 100' });
-  } else {
-    const sql = `
-      insert into "grades" ("name", "course", "score")
-        values ($1, $2, $3)
-      returning *
-    `;
-    const values = [name, course, score];
-    db.query(sql, values)
-      .then(result => {
-        const grade = result.rows[0];
-        res.status(201).json(grade);
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: 'An unexpected error occured' });
-      });
+    return;
   }
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+      values ($1, $2, $3)
+    returning *
+  `;
+  const values = [name, course, score];
+  db.query(sql, values)
+    .then(result => {
+      const grade = result.rows[0];
+      res.status(201).json(grade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occured' });
+    });
 });
 
 app.put('/api/grades/:gradeId', (req, res) => {
   const gradeId = Number(req.params.gradeId);
   const input = req.body;
-  const [name, course, score] = [input.name, input.course, Number(input.score)];
+  const name = input.name;
+  const course = input.course;
+  const score = input.score;
   if (!Number.isInteger(gradeId) || gradeId < 0) {
     res.status(400).json({ error: 'invalid gradeId input' });
-  } else if (!name || !course || !score) {
-    res.status(400).json({ error: 'Must include Name, Course, and Score.' });
-  } else if (!Number.isInteger(score) || score < 1 || score > 100) {
-    res.status(400).json({ error: 'Score must be a positive integer between 1 and 100' });
-  } else {
-    const sql = `
-      update "grades"
-        set  "name" = $1,
-            "course" = $2,
-            "score" = $3
-      where "gradeId" = $4
-      returning *
-    `;
-    const values = [name, course, score, gradeId];
-    db.query(sql, values)
-      .then(result => {
-        const grade = result.rows[0];
-        if (!grade) {
-          res.status(404).json({ error: 'cannot find grade with gradeId of ' + gradeId });
-        } else {
-          res.status(200).json(grade);
-        }
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: 'An unexpected error occured' });
-      });
+    return;
   }
+  if (score < 0 || score > 100 || Number.isNaN(score)) {
+    res.status(400).json({ error: 'Score must be a positive integer between 0 and 100' });
+    return;
+  }
+  if (!name || !course || !score) {
+    res.status(400).json({ error: 'Must include Name, Course, and Score.' });
+    return;
+  }
+  const sql = `
+    update "grades"
+      set  "name" = $1,
+          "course" = $2,
+          "score" = $3
+    where "gradeId" = $4
+    returning *
+  `;
+  const values = [name, course, score, gradeId];
+  db.query(sql, values)
+    .then(result => {
+      const grade = result.rows[0];
+      if (!grade) {
+        res.status(404).json({ error: 'cannot find grade with gradeId of ' + gradeId });
+      } else {
+        res.status(200).json(grade);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occured' });
+    });
 });
 
 app.delete('/api/grades/:gradeId', (req, res) => {

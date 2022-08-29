84714379d367a144dac0256744315599e84e4c41
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-29 17:54:52 +0000

    Completed ES6-Classes Exercise

---
+++ es6-classes/student.js
@@ -0,0 +1,18 @@
+class Student {
+  constructor(firstName, lastName, subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getIntroduction() {
+    return `Hello, my name is ${this.firstName} ${this.lastName} and I am studying ${this.subject}.`;
+  }
+}
+
+// eslint-disable-next-line no-console
+console.log(Student);

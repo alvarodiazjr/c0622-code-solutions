44f52707c030b66fd5f8b9e735649b289b789fa9
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-08-31 19:54:04 +0000

    Completed React-Hot-Button-Component Challenge

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HotButton from './hot-button';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+const element = <HotButton/>;
+
+root.render(element);

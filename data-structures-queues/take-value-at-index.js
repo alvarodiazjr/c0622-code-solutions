d3150116676f3cef708efb601d72e5ac12c7956f
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-09-27 22:16:22 +0000

    Completed Data-structures-queues Exercise

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,8 +1,11 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
-  // if (queue.peek() === undefined) return;
-  // for (let i = 0; i < index; i++) {
-
-  // }
+  if (queue.peek() === undefined) return;
+  for (let i = 0; i < index; i++) {
+    const removedVal = queue.dequeue();
+    queue.enqueue(removedVal);
+  }
+  const value = queue.dequeue();
+  return value;
 }
